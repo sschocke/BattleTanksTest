b6b2ac4a1f043b411010d48b28d57f65aab5a212
Sebastian Schocke <huntrckr@gmail.com>
2013-07-15 07:01:36 +0200

    Added AI Debug Windows

---
+++ BattleTanksTest/AIDebugWindow.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BattleTanksTest
+{
+    partial class AIDebugWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(300, 637);
+            this.txtLog.TabIndex = 0;
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(300, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(656, 637);
+            this.canvas.TabIndex = 1;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // AIDebugWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 637);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.txtLog);
+            this.Name = "AIDebugWindow";
+            this.Text = "Debug Window";
+            this.SizeChanged += new System.EventHandler(this.AIDebugWindow_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.PictureBox canvas;
+    }
+}
---
+++ BattleTanksTest/AIDebugWindow.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BattleTanksTest
+{
+    public partial class AIDebugWindow : Form
+    {
+        private int myPlayerIdx;
+        private ClientGameState state = null;
+        private Rectangle gridRect;
+
+        public AIDebugWindow(string playerName, int playerIdx)
+        {
+            InitializeComponent();
+
+            this.Text = "Debug Window - " + playerName;
+            this.myPlayerIdx = playerIdx;
+            this.addLogDel = new AddLogDelegate(this.AddLog);
+        }
+
+        public void UpdateState(ClientGameState newState)
+        {
+            this.state = newState;
+            canvas.Invalidate();
+        }
+        private delegate void AddLogDelegate(string text);
+
+        private AddLogDelegate addLogDel;
+        public void AddLog(string text)
+        {
+            if (this.txtLog.InvokeRequired)
+            {
+                this.txtLog.Invoke(this.addLogDel, new object[] { text });
+            }
+            else
+            {
+                this.txtLog.AppendText(text + Environment.NewLine);
+            }
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (this.state == null)
+            {
+                return;
+            }
+
+            int gridWidth = (canvas.Width - 10) / this.state.BoardWidth;
+            int gridHeight = (canvas.Height - 10) / this.state.BoardHeight;
+
+            gridRect = new Rectangle(5, 5, gridWidth, gridHeight);
+            for (int y = 0; y < this.state.BoardHeight; y++)
+            {
+                gridRect.X = 5;
+                for (int x = 0; x < this.state.BoardWidth; x++)
+                {
+                    if (this.state.blocks[x, y] == State.FULL)
+                    {
+                        e.Graphics.FillRectangle(Brushes.DarkGray, gridRect);
+                    }
+                    if ((this.state.players[0].playerBase.x == x) &&
+                        (this.state.players[0].playerBase.y == y))
+                    {
+                        e.Graphics.FillRectangle((0 == myPlayerIdx ? Brushes.Green : Brushes.Red), gridRect);
+                    }
+                    if ((this.state.players[1].playerBase.x == x) &&
+                        (this.state.players[1].playerBase.y == y))
+                    {
+                        e.Graphics.FillRectangle((1 == myPlayerIdx ? Brushes.Green : Brushes.Red), gridRect);
+                    }
+                    e.Graphics.DrawRectangle(Pens.Black, gridRect);
+                    gridRect.Offset(gridWidth, 0);
+                }
+                gridRect.Offset(0, gridHeight);
+            }
+            for (int p = 0; p < this.state.players.Length; p++)
+            {
+                foreach (Unit unit in this.state.players[p].units)
+                {
+                    drawUnit(e.Graphics, gridWidth, gridHeight, unit, (p == myPlayerIdx ? Brushes.Green : Brushes.Red));
+                }
+                foreach (Bullet bullet in this.state.players[p].bullets)
+                {
+                    drawBullet(e.Graphics, gridWidth, gridHeight, bullet, (p == myPlayerIdx ? Brushes.Green : Brushes.Red));
+                }
+            }
+        }
+
+        public static void drawUnit(Graphics g, int gridWidth, int gridHeight, Unit unit, Brush color)
+        {
+            int unitX = 5 + (unit.x * gridWidth) + (int)(gridWidth * 0.5);
+            int unitY = 5 + (unit.y * gridHeight) + (int)(gridHeight * 0.5);
+            Rectangle unitBaseRect = new Rectangle(unitX - (int)(gridWidth * 2.4), unitY - (int)(gridHeight * 2.4), (int)(gridWidth * 4.8), (int)(gridHeight * 4.8));
+            Rectangle unitGunRect = new Rectangle(unitX - (int)(gridWidth * 0.2), unitY - (int)(gridHeight * 0.2), (int)(gridWidth * 0.4), (int)(gridHeight * 0.4));
+            Rectangle unitHatchRect = new Rectangle(unitX - (int)(gridWidth * 0.4), unitY - (int)(gridHeight * 0.4), (int)(gridWidth * 0.8), (int)(gridHeight * 0.8));
+            switch (unit.direction)
+            {
+                case Direction.UP:
+                    unitGunRect.Y -= (int)(gridHeight * 1.8);
+                    unitGunRect.Height = (int)(gridHeight * 2);
+                    break;
+                case Direction.DOWN:
+                    unitGunRect.Height = (int)(gridHeight * 2);
+                    break;
+                case Direction.LEFT:
+                    unitGunRect.X -= (int)(gridWidth * 1.8);
+                    unitGunRect.Width = (int)(gridWidth * 2);
+                    break;
+                case Direction.RIGHT:
+                    unitGunRect.Width = (int)(gridWidth * 2);
+                    break;
+            }
+            g.FillRectangle(color, unitBaseRect);
+            g.FillRectangle(Brushes.Black, unitGunRect);
+            g.FillEllipse(color, unitHatchRect);
+            g.DrawEllipse(Pens.Black, unitHatchRect);
+        }
+        public static void drawBullet(Graphics g, int gridWidth, int gridHeight, Bullet bullet, Brush color)
+        {
+            int bulletX = 5 + (bullet.x * gridWidth) + (int)(gridWidth * 0.5);
+            int bulletY = 5 + (bullet.y * gridHeight) + (int)(gridHeight * 0.5);
+            Rectangle bulletBaseRect = new Rectangle(bulletX - (int)(gridWidth * 0.4), bulletY - (int)(gridHeight * 0.4), (int)(gridWidth * 0.8), (int)(gridHeight * 0.8));
+            g.FillEllipse(color, bulletBaseRect);
+        }
+
+        private void AIDebugWindow_SizeChanged(object sender, EventArgs e)
+        {
+            canvas.Invalidate();
+        }
+    }
+}
